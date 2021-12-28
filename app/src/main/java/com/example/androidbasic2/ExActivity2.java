e80f0489ede215e320dd0ee5bd189319030c0596
DESKTOP-319ET69\admin <haimh@gviet.vn>
2021-12-28 08:24:55 +0700

    update

--- app/src/main/java/com/example/androidbasic2/ExActivity2.java
+++ app/src/main/java/com/example/androidbasic2/ExActivity2.java
@@ -3,6 +3,8 @@ package com.example.androidbasic2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class ExActivity2 extends AppCompatActivity {
 
@@ -11,4 +13,5 @@ public class ExActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex2);
     }
+
 }
